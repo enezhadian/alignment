28d7f779b6f76e7dc387d5ab28a765e04439d859
Ehsan Nezhadian <enezhadian@gmail.com>
2018-10-06 02:08:02 +0200

    Scripts

--- scripts/qsaff.sh
+++ scripts/qsaff.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 #block(name=strong_affine, threads=2, memory=7500, hours=1000, gpus=1)
-    python3 -m train_strong                                 \
-        --feature-extraction-cnn        50_1000_full_20     \
-        --feature-extraction-last-layer layer3              \
+    python3 -m train_strong                                                           \
+        --feature-extraction-cnn        50_1000_full_20                               \
+        --feature-extraction-last-layer layer3                                        \
         --delf-path ../delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar \
-        --training-dataset              pascal              \
-        --dataset-csv-path              data/pascal-random/ \
-        --num-epochs                    30                  \
-        --batch-size                    32                  \
-        --random-sample                 1                   \
+        --training-dataset              pascal                                        \
+        --dataset-csv-path              training_data/pascal-random/                  \
+        --num-epochs                    30                                            \
+        --batch-size                    32                                            \
+        --random-sample                 1                                             \
         --result-model-fn               delf
 
-    python3 -m train_strong                                 \
+    python3 -m train_strong                                                                        \
         --model trained_models/best_delf_strong_50_pascal_affine_50_1000_full_20_grid_loss.pth.tar \
-        --feature-extraction-cnn        50_1000_full_20     \
-        --feature-extraction-last-layer layer3              \
-        --delf-path ../delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar \
-        --training-dataset              pascal              \
-        --dataset-csv-path              data/pascal-random/ \
-        --num-epochs                    30                  \
-        --batch-size                    32                  \
-        --random-sample                 1                   \
-        --lr                            0.0002              \
+        --feature-extraction-cnn        50_1000_full_20                                            \
+        --feature-extraction-last-layer layer3                                                     \
+        --delf-path ../delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar              \
+        --training-dataset              pascal                                                     \
+        --dataset-csv-path              training_data/pascal-random/                               \
+        --num-epochs                    30                                                         \
+        --batch-size                    32                                                         \
+        --random-sample                 1                                                          \
+        --lr                            0.0002                                                     \
         --result-model-fn               delf_smallerlr
--- scripts/qw.sh
+++ scripts/qw.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 #block(name=weak_delf, threads=2, memory=7500, hours=1000, gpus=1)
-    python3 -m train_weak                                    \
+    python3 -m train_weak                                                                                \
         --model-aff models/best_delf_smallerlr_strong_50_pascal_affine_50_1000_full_20_grid_loss.pth.tar \
-        --model-tps models/best_delf_smallerlr_strong_50_pascal_tps_50_1000_full_20_grid_loss.pth.tar \
-        --feature-extraction-cnn        50_1000_full_20      \
-        --feature-extraction-last-layer layer3               \
-        --delf-path ../delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar \
-        --training-dataset              pf-pascal            \
-        --dataset-csv-path              data/pf-pascal-flip/ \
-        --dataset-image-path            data/pfpascal        \
-        --num-epochs                    30                   \
-        --lr                            5e-8                 \
+        --model-tps models/best_delf_smallerlr_strong_50_pascal_tps_50_1000_full_20_grid_loss.pth.tar    \
+        --feature-extraction-cnn        50_1000_full_20                                                  \
+        --feature-extraction-last-layer layer3                                                           \
+        --delf-path ../delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar                    \
+        --training-dataset              pf-pascal                                                        \
+        --dataset-csv-path              training_data/pf-pascal-flip/                                    \
+        --dataset-image-path            training_data/pfpascal                                           \
+        --num-epochs                    30                                                               \
+        --lr                            5e-8                                                             \
         --result-model-fn               delf_weak
