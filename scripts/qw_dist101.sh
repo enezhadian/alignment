9e8c822e7d4076c99bf86dce1f13621f4e7ed5eb
Ehsan Nezhadian <enezhadian@gmail.com>
2018-10-09 09:35:38 +0000

    Scripts

---
+++ scripts/qw_dist101.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#block(name=weak_delf, threads=2, memory=7500, hours=1000, gpus=1)
+    python3 -m train_weak                                                                                        \
+        --model-aff trained_models/best_delf_dist101_smallerlr_strong_30_pascal_affine_101_1000_full_1000_grid_loss.pth.tar \
+        --model-tps trained_models/best_delf_dist101_smallerlr_strong_30_pascal_tps_101_1000_full_1000_grid_loss.pth.tar    \
+        --feature-extraction-cnn        101_1000_full_1000                                                          \
+        --feature-extraction-last-layer layer3                                                                   \
+        --delf-path                     trained_models/delf_dist101_converted.pth.tar                            \
+        --training-dataset              pf-pascal                                                                \
+        --dataset-csv-path              training_data/pf-pascal-flip/                                            \
+        --dataset-image-path            datasets/proposal-flow-pascal                                            \
+        --num-epochs                    30                                                                       \
+        --lr                            5e-8                                                                     \
+	--result-model-fn               delf_dist101_weak
