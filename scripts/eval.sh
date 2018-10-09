e9c38b9b31ac7881c2e86a868cec588b6d8b904f
Ehsan Nezhadian <enezhadian@gmail.com>
2018-10-09 20:55:16 +0200

    Scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,7 +6,13 @@
 #       --model                  ../final_evaluations/best_delf_weak_50_1000_full_20_regfact0.2.pth.tar             \
 #       --delf-path              ../selected_models/delf_ep10_bz32_lr0.001_im224_taken/20180927113241/best.pth.tar  \
 #       --eval-dataset           pf-pascal
+#     python eval.py                                                                                                \
+#       --feature-extraction-cnn resnet101                                                                          \
+#       --model                  ../selected_models/weak_resnet101/resnet101_weak_resnet101_regfact0.2.pth.tar      \
+#       --eval-dataset           pf-pascal
     python eval.py                                                                                                \
-      --feature-extraction-cnn resnet101                                                                          \
-      --model                  ../selected_models/weak_resnet101/resnet101_weak_resnet101_regfact0.2.pth.tar      \
+      --feature-extraction-cnn 101_1000_full_1000                                                                 \
+      --model                  ../final_evaluations/best_delf_dist101_weak_101_1000_full_1000_regfact0.2.pth.tar  \
+      --delf-path              ../selected_models/delf_dist101_converted.pth.tar                                  \
       --eval-dataset           pf-pascal
+
